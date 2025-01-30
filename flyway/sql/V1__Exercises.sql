0b206118f5bb80308cf1626226e28e07d30077d0
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-01-30 10:23:26 +0200

    Changed database table

--- flyway/sql/V1__Exercises.sql
+++ flyway/sql/V1__Exercises.sql
@@ -1,12 +1,11 @@
 CREATE TABLE exercises (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL PRIMARY KEY,
     category TEXT CHECK (category IN ('Barbell','Dumbbell','Machine','Bodyweight','Cable','Other'))
 );
 
 CREATE TABLE muscle_groups (
-    exercise_id INTEGER NOT NULL,
+    exercise_name TEXT NOT NULL,
     muscle TEXT CHECK (muscle IN ('Chest','Upper Chest','Abs','Quads','Hamstrings','Calves','Forearms','Biceps','Triceps','Traps','Upper Back','Lower Back','Lats','Rear Delt','Side Delt','Front Delt','Neck','Glutes')),
-    PRIMARY KEY (exercise_id, muscle),
-    FOREIGN KEY (exercise_id) REFERENCES exercises(id)
+    PRIMARY KEY (exercise_name, muscle),
+    FOREIGN KEY (exercise_name) REFERENCES exercises(name)
 );

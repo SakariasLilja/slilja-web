84ad7f391afc03d0914470430fa97984ed5f4907
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-02-14 14:40:34 +0200

    Fixed database migration

--- flyway/sql/V1__Exercises.sql
+++ flyway/sql/V1__Exercises.sql
@@ -1,21 +1,20 @@
 CREATE TABLE users (
     email TEXT NOT NULL PRIMARY KEY,
     password TEXT NOT NULL
-)
+);
 
 CREATE TABLE exercises (
-    user TEXT NOT NULL,
+    user_email TEXT NOT NULL,
     name TEXT NOT NULL,
     category TEXT CHECK (category IN ('Barbell','Dumbbell','Machine','Bodyweight','Cable','Other')),
-    PRIMARY Key (user, name),
-    FOREIGN KEY (user) REFERENCES users(email)
+    PRIMARY Key (user_email, name),
+    FOREIGN KEY (user_email) REFERENCES users(email)
 );
 
 CREATE TABLE muscle_groups (
-    user TEXT NOT NULL,
+    user_email TEXT NOT NULL,
     exercise_name TEXT NOT NULL,
     muscle TEXT CHECK (muscle IN ('Chest','Upper Chest','Abs','Quads','Hamstrings','Calves','Forearms','Biceps','Triceps','Traps','Upper Back','Lower Back','Lats','Rear Delt','Side Delt','Front Delt','Neck','Glutes')),
-    PRIMARY KEY (user, exercise_name, muscle),
-    FOREIGN KEY (exercise_name) REFERENCES exercises(name),
-    FOREIGN KEY (user) REFERENCES users(email)
+    PRIMARY KEY (user_email, exercise_name, muscle),
+    FOREIGN KEY (user_email, exercise_name) REFERENCES exercises(user_email, name)
 );

b0512871477beadb531d77663ab982863035001b
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-02-15 21:45:15 +0200

    Updated database

--- flyway/sql/V1__Exercises.sql
+++ flyway/sql/V1__Exercises.sql
@@ -1,20 +1,23 @@
 CREATE TABLE users (
-    email TEXT NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
 CREATE TABLE exercises (
-    user_email TEXT NOT NULL,
+    user_id INTEGER,
     name TEXT NOT NULL,
     category TEXT CHECK (category IN ('Barbell','Dumbbell','Machine','Bodyweight','Cable','Other')),
-    PRIMARY Key (user_email, name),
-    FOREIGN KEY (user_email) REFERENCES users(email)
+    PRIMARY Key (user_id, name),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE muscle_groups (
-    user_email TEXT NOT NULL,
+    user_id INTEGER,
     exercise_name TEXT NOT NULL,
     muscle TEXT CHECK (muscle IN ('Chest','Upper Chest','Abs','Quads','Hamstrings','Calves','Forearms','Biceps','Triceps','Traps','Upper Back','Lower Back','Lats','Rear Delt','Side Delt','Front Delt','Neck','Glutes')),
-    PRIMARY KEY (user_email, exercise_name, muscle),
-    FOREIGN KEY (user_email, exercise_name) REFERENCES exercises(user_email, name)
+    PRIMARY KEY (user_id, exercise_name, muscle),
+    FOREIGN KEY (user_id, exercise_name) REFERENCES exercises(user_id, name)
 );
+
+CREATE UNIQUE INDEX ON users(lower(email));

240d9678fcd564fc46861a55b934d810a5b65e80
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-02-16 00:53:25 +0200

    Removed definitions

--- src/app/lib/definitions.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const RegisterUserSchema = z.object({
-    email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
-    password: z
-        .string()
-        .min(6, { message: 'Be at least 6 characters long' })
-        .max(64, { message: 'Be at most 64 characters long' })
-})

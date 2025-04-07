7bc89799a0e3c4b404af2d3cd2609ed95c5801a8
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-04-08 01:32:11 +0300

    Added authentication services

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,17 @@
 import { betterAuth } from "better-auth";
 import { db } from "@/database";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    database: db,
+    database: {
+        db: db,
+        type: "postgres"
+    },
     emailAndPassword: {
         enabled: true
     },
     user: {
         modelName: "app_user"
-    }
+    },
+    plugins: [nextCookies()]
 })
